a770d2bae99dd266dd103af009964d3424988cc7
lmfy <2861782815@qq.com>
2025-02-28 14:43:33 +0800

    Updates

---
+++ _next/static/wHW4XxKWkPPZXSxmihexy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/125-0102ed9d0399fdf5.js","static/chunks/pages/index-bd83052e8cf2898d.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
