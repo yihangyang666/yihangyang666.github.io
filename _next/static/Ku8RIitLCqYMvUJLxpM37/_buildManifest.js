ef6f24d6c0278c7a82d1555c66c633331a64cb4e
lmfy <2861782815@qq.com>
2025-02-28 23:00:28 +0800

    Updates

---
+++ _next/static/Ku8RIitLCqYMvUJLxpM37/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/125-0102ed9d0399fdf5.js","static/chunks/pages/index-8955d6f927a55c50.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
